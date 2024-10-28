8608ee0723fe51a1ded71436d2b199db4c01b559
Erkan Tan <rkntan@gmail.com>
2024-10-28 16:09:55 +0100

    added isogram

---
+++ exercisim-c-track/isogram/README.md
@@ -0,0 +1,32 @@
+# isogram
+
+This repository contains a solution for the Two Fer exercise from Exercism in C.
+For more details, visit the [exercise page](https://exercism.org/tracks/c/exercises/isogram/).
+
+## problem description
+
+Determine if a word or phrase is an isogram.
+
+An isogram (also known as a "non-pattern word") is a word or phrase without a repeating letter, however spaces and hyphens are allowed to appear multiple times.
+
+Examples of isograms:
+
+lumberjacks
+background
+downstream
+six-year-old
+The word isograms, however, is not an isogram, because the s repeats.
+
+#
+
+1. Build the project using `make`:
+    ```
+    make
+    ```
+
+2. Run the tests:
+    ```
+    .\test_isogram.exe
+    ```
+
+
---
+++ exercisim-c-track/isogram/src/isogram.c
@@ -0,0 +1,43 @@
+#include "isogram.h"
+#include "stdint.h"
+
+bool is_isogram(const char phrase[])
+{
+
+    if (phrase == NULL)
+    {
+        return false;
+    }
+
+    uint32_t flags = 0;
+
+    for (int i = 0; phrase[i] != '\0'; i++)
+    {
+        int pos;
+        if (phrase[i] >= 'A' && phrase[i] <= 'Z')
+        {
+            pos = phrase[i] - 'A';
+        }
+        else if (phrase[i] >= 'a' && phrase[i] <= 'z')
+        {
+            pos = phrase[i] - 'a';
+        }
+        else if (phrase[i] == ' ' || phrase[i] == '-')
+        {
+            continue; // NOP
+        }
+        else
+        {
+            return false; // invalid characters
+        }
+
+        if ((flags & (1 << pos)) != 0)
+        {
+            return false;
+        }
+
+        flags |= (1 << pos);
+    }
+
+    return true;
+}
